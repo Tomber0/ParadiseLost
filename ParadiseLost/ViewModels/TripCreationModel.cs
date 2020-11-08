d946c559d7209b0b9513d48f040e66dfa1bffb48
Алексей Зайцев <llzaicev@yandex.ru>
2020-11-09 01:15:14 +0300

    Trip creation model added

---
+++ ParadiseLost/ViewModels/TripCreationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParadiseLost.ViewModels
+{
+    public class TripCreationModel
+    {
+
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Укажите название!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указано описание!")]
+        public string Description { get; set; }
+        public string Location { get; set; }
+
+        public bool Visible { get; set; }
+
+        public string Tags { get; set; }
+
+    }
+}
